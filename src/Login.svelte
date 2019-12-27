cfb6195bfb909de61ea30d05af071cdb9fb5b0aa
Alexandr Dmowski <admowski@aras.com>
2019-12-27 18:17:03 +0300

    Login page

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,9 +1,15 @@
 ﻿<script>
 	import sendRequest from './login';
+	import { credentials } from './stores.js';
 
 	async function click() {
 		await sendRequest();
 	}
+
+	function setCredentials() {
+		credentials.setCredentials();
+	}
 </script>
 
 <button on:click={click}>Send request</button>
+<button on:click={setCredentials}>setCredentials</button>

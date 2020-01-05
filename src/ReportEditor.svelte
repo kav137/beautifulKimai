670d8601838c260aaea6da579df026653a6b2389
Alexandr Dmowski <dmowski@yandex.ru>
2020-01-05 09:54:04 +0300

    Remove test templater for CRUD Add module for edit report

---
+++ src/ReportEditor.svelte
@@ -0,0 +1,35 @@
+<div>
+  <form>
+    <label>
+      <span>Description</span>
+      <textarea name="description" id="" cols="30" rows="10" />
+    </label>
+
+    <label>
+      <span>Date</span>
+      <input type="text" name="date" />
+    </label>
+
+    <label>
+      <span>Duration</span>
+      <input type="text" name="duration" />
+    </label>
+
+    <label>
+      <span>Customer</span>
+      <input type="text" name="customer" />
+    </label>
+
+    <label>
+      <span>Project</span>
+      <input type="text" name="project" />
+    </label>
+
+    <label>
+      <span>Activity</span>
+      <input type="text" name="activity" />
+    </label>
+
+    <input type="submit" value="Save" />
+  </form>
+</div>
--- src/Reports.svelte
+++ src/Reports.svelte
@@ -1,57 +1,11 @@
 <script>
   import reports from "./store/reportsStore.js";
   import PreviewReport from "./PreviewReport.svelte";
+  import ReportEditor from "./ReportEditor.svelte";
+
   import { onMount } from "svelte";
 
-  let people = [
-    { first: "Hans", last: "Emil" },
-    { first: "Max", last: "Mustermann" },
-    { first: "Roman", last: "Tisch" }
-  ];
-
-  let prefix = "";
-  let first = "";
-  let last = "";
-  let i = 0;
-
-  $: filteredPeople = prefix
-    ? people.filter(person => {
-        const name = `${person.last}, ${person.first}`;
-        return name.toLowerCase().startsWith(prefix.toLowerCase());
-      })
-    : people;
-
-  $: selected = filteredPeople[i];
-
   $: selectedReport = null;
-
-  $: reset_inputs(selected);
-
-  function create() {
-    people = people.concat({ first, last });
-    i = people.length - 1;
-    first = last = "";
-  }
-
-  function update() {
-    selected.first = first;
-    selected.last = last;
-    people = people;
-  }
-
-  function remove() {
-    // Remove selected person from the source array (people), not the filtered array
-    const index = people.indexOf(selected);
-    people = [...people.slice(0, index), ...people.slice(index + 1)];
-
-    first = last = "";
-    i = Math.min(i, filteredPeople.length - 2);
-  }
-
-  function reset_inputs(person) {
-    first = person ? person.first : "";
-    last = person ? person.last : "";
-  }
 
   function selectReport(event) {
     const selectedId = event.detail.id;
@@ -63,42 +17,40 @@
   });
 </script>
 
-<div>
-  {#if selectedReport !== null}
-    <hr />
-    <h1>Selected report</h1>
-    <PreviewReport report={selectedReport} />
-  {/if}
+<style>
+  .list {
+    width: 50%;
+    padding: 10px;
+    box-sizing: border-box;
+  }
+
+  .report-editor {
+    box-sizing: border-box;
+    padding: 10px;
+    width: 50%;
+    position: fixed;
+    right: 0;
+    top: 0;
+    height: 100vh;
+  }
+</style>
+
+<div class="list">
+  <h1>Reports</h1>
+  {#each $reports as report, i}
+    <PreviewReport
+      {report}
+      isSelectedReport={selectedReport && selectedReport.id === report.id}
+      on:select={selectReport} />
+  {/each}
 </div>
 
-<hr />
-<h1>Reports</h1>
-{#each $reports as report, i}
-  <PreviewReport
-    {report}
-    isSelectedReport={selectedReport && selectedReport.id === report.id}
-    on:select={selectReport} />
-{/each}
+<div class="report-editor">
+  <h1>Report editor</h1>
 
-<input placeholder="filter prefix" bind:value={prefix} />
-
-<select bind:value={i} size={5}>
-  {#each filteredPeople as person, i}
-    <option value={i}>{person.last}, {person.first}</option>
-  {/each}
-</select>
-
-<label>
-  <input bind:value={first} placeholder="first" />
-</label>
-<label>
-  <input bind:value={last} placeholder="last" />
-</label>
-
-<div class="buttons">
-  <button on:click={create} disabled={!first || !last}>create</button>
-  <button on:click={update} disabled={!first || !last || !selected}>
-    update
-  </button>
-  <button on:click={remove} disabled={!selected}>delete</button>
+  {#if selectedReport !== null}
+    <ReportEditor report={selectedReport} />
+  {:else}
+    <p>Need select report</p>
+  {/if}
 </div>

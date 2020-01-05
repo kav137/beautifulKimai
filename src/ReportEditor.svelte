5fef3c5d0ea9f4f9d894ca4c793f98920a5e23e9
Alexandr Dmowski <dmowski@yandex.ru>
2020-01-05 10:08:52 +0300

    Add some style for editor of report

--- src/ReportEditor.svelte
+++ src/ReportEditor.svelte
@@ -1,33 +1,44 @@
+<script>
+  export let report;
+</script>
+
+<style>
+  textarea {
+    width: 100%;
+    height: 150px;
+  }
+  span {
+    min-width: 100px;
+    display: inline-block;
+  }
+</style>
+
 <div>
   <form>
     <label>
-      <span>Description</span>
-      <textarea name="description" id="" cols="30" rows="10" />
+      <span>Description:</span>
+      <br />
+      <textarea bind:value={report.description} name="description" />
     </label>
 
     <label>
-      <span>Date</span>
-      <input type="text" name="date" />
+      <span>Date:</span>
+      <input bind:value={report.begin} type="text" name="date" />
     </label>
 
     <label>
-      <span>Duration</span>
-      <input type="text" name="duration" />
+      <span>Duration:</span>
+      <input bind:value={report.duration} type="text" name="duration" />
     </label>
 
     <label>
-      <span>Customer</span>
-      <input type="text" name="customer" />
+      <span>Project:</span>
+      <input bind:value={report.project.name} type="text" name="project" />
     </label>
 
     <label>
-      <span>Project</span>
-      <input type="text" name="project" />
-    </label>
-
-    <label>
-      <span>Activity</span>
-      <input type="text" name="activity" />
+      <span>Activity:</span>
+      <input bind:value={report.activity.name} type="text" name="activity" />
     </label>
 
     <input type="submit" value="Save" />

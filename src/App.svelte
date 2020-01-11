07aaf11051a0ea4dafb876259235e18027690a50
Alexandr Dmowski <dmowski@yandex.ru>
2020-01-11 15:52:37 +0300

    Create logout button

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,26 @@
   import Login from "./Login.svelte";
   import Reports from "./Reports.svelte";
   import credentials from "./store/credentialsStore.js";
+
+  function logout() {
+    credentials.logout();
+  }
 </script>
+
+<style>
+  .logout {
+    position: fixed;
+    right: 10px;
+    top: 10px;
+    z-index: 1;
+  }
+</style>
 
 <main>
   {#if $credentials === null}
     <Login />
   {:else}
+    <button class="logout" on:click={logout}>Logout</button>
     <Reports />
   {/if}
 </main>
